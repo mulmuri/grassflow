b9747fee067b10ea6894cfa0d583e9fa6a4997a5
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-04-28 01:50:39 +0900

    add argument error handling logic

--- cmd/run.go
+++ cmd/run.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/mulmuri/grassflow/cmd/execute"
+	"github.com/mulmuri/grassflow/cmd/form"
 	"github.com/mulmuri/grassflow/cmd/help"
 	"github.com/mulmuri/grassflow/cmd/rollback"
 	"github.com/mulmuri/grassflow/cmd/static"
 	"github.com/mulmuri/grassflow/cmd/version"
-	"github.com/mulmuri/grassflow/cmd/form"
 )
 
 
@@ -19,8 +20,14 @@ func main() {
 
 	var err error
 
-	workflowName := os.Args[1]
-	newArgs := os.Args[2:]
+	defer func() {
+		if err := recover(); err != nil {
+			log.Fatal(fmt.Errorf("panic recovered: %v", err))
+		}
+	}()
+
+	workflowName := os.Args[0]
+	newArgs := os.Args[1:]
 
 	if (newArgs[0] == "-v" || newArgs[0] == "--version" || newArgs[0] == "v") {
 		workflowName = "version"
@@ -51,6 +58,6 @@ func main() {
 	}
 
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(fmt.Errorf("failed to run workflow: %v", err))
 	}
 }
